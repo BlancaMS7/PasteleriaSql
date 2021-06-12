d6d002d9332d7db84552b9e79349e5d3bf8d6f0e
BlancaMS7 <85451335+BlancaMS7@users.noreply.github.com>
2021-06-12 03:59:49 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,3 @@
+insert into [PasteleriaSql].[dbo].[Consumiciones](PrecioConsumicion, cantidadConsumicion, idClienteFk5, idTrabajadorFk6) values ('20','12','1','2'),('40','20','2','3'),('30','1','3','1');
+
+select * from Consumiciones;
